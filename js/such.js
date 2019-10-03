79b24c694cdc771922a556ba8bd03a6d2f5f395d
Nicole <nicole.fischli@hotmail.com>
2019-10-03 19:43:02 +0200

    geändert

--- js/such.js
+++ js/such.js
@@ -1,7 +1,9 @@
-console.log('hallo');
-document.querySelector('suchbutton').addEventListener('click', function(){
+
+
+document.querySelector('#suchbutton').addEventListener('click', function(e){
+    e.preventDefault();
   
-    let myInput =  document.querySelector('suchinput').value
+    let myInput =  document.querySelector('#suchinput').value
   
   
     fetch(`https://api.unsplash.com/search/photos/?client_id=6b99c44af3b5ecf26972b41b9e4651ec31dfc4a8c13be8a8ca48eb34d8c4f8da&query=${myInput}`)
@@ -9,6 +11,12 @@ document.querySelector('suchbutton').addEventListener('click', function(){
       response.json().then(json => {
         console.log(json);
         
+        for(x = 0; x <= 10; x++){
+            
+            document.querySelector('#bildcontainer').appendChild()
+
+        }
+
       });
     });
   
